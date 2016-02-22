9f5fc695d451f89a859c4f6d732056f144267ca2
Koprvhdix <454898060@qq.com>
2016-02-22 21:53:02 +0800

    base commit

--- learn_SQLAlchemy.py
+++ learn_SQLAlchemy.py
@@ -1,4 +1,4 @@
-# coding: UTF-8
+# coding=utf-8
 # Ubuntu
 # 安装MySQL：
 #   apt-get install mysql-server
@@ -9,25 +9,45 @@
 # 安装SQLAlchemy
 #   easy_install SQLAlchemy
 
-from sqlalchemy import create_engine, Column, String
+from sqlalchemy import create_engine, Column, String, ForeignKey
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class User(Base):
+class user_list(Base):
 	__tablename__ = 'user'
 
-	id = Column(String(20), primary_key=True)
-	name = Column(String(20))
+	user_id = Column(String(20), primary_key=True)
+	user_name = Column(String(20))
 
-# mysql+mysqldb://用户名:密码@主机名:端口号/数据库
-engine = create_engine('mysql+mysqldb://root:123456@localhost:3306/test')
-DBSession = sessionmaker(bind=engine)
 
-session = DBSession()
+class special_user_list(Base):
+	__tablename__ = 'special_user'
 
-new_user = User(id='5', name='Bob')
-session.add(new_user)
-session.commit()
-session.close()
+	# ForeignKey must be in front of primary_key
+	special_user_id = Column(String(20), ForeignKey('user.user_id'), primary_key=True)
+
+# 创建所有表格
+def create_All(engine):
+    Base.metadata.create_all(engine)
+
+# 删除所有表格
+def drop_All(engine):
+    Base.metadata.drop_all(engine)
+
+if __name__ == '__main__':
+	# mysql+mysqldb://用户名:密码@主机名:端口号/数据库
+	engine = create_engine('mysql+mysqldb://root:123456@localhost:3306/test')
+	DBSession = sessionmaker(bind=engine)
+
+	session = DBSession()
+
+	drop_All(engine)
+
+	create_All(engine)
+
+	new_user = User(id='5', name='Bob')
+	session.add(new_user)
+	session.commit()
+	session.close()
